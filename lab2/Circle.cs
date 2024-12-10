2645a7b979d2fcf665c8af1dfc73a0c81b32b04c
colan1ch <nachernev@gmail.com>
2024-12-10 20:47:20 +0300

    lab2 added

---
+++ lab2/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    class Circle : Figure, IPrint
+    {
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        private double _radius;
+
+        public double radius
+        {
+            get => _radius;
+            set
+            {
+                if (value > 0)
+                {
+                    _radius = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Основной конструктор
+        /// </summary>
+        /// <param name="ph">Высота</param>
+        /// <param name="pw">Ширина</param>
+        public Circle(double pr)
+        {
+            this.radius = pr;
+            this.Type = "Круг";
+        }
+
+        public override double Area()
+        {
+            return Math.PI * this.radius * this.radius;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
---
+++ lab2/Rectangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    class Rectangle : Figure, IPrint
+    {
+        /// <summary>
+        /// Высота
+        /// </summary>
+        private double _height;
+
+        public double height
+        {
+            get => _height;
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        private double _width;
+
+        public double width
+        {
+            get => _width;
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Основной конструктор
+        /// </summary>
+        /// <param name="ph">Высота</param>
+        /// <param name="pw">Ширина</param>
+        public Rectangle(double ph, double pw)
+        {
+            this.height = ph;
+            this.width = pw;
+            this.Type = "Прямоугольник";
+        }
+
+        /// <summary>
+        /// Вычисление площади
+        /// </summary>
+        public override double Area()
+        {
+            return this.width * this.height;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
